070f065c7b9534202f70780e2ea4e5b253f66e00
mahimkarrohit7 <mahimkarrohit7@gmail.com>
2026-02-22 16:00:59 +0530

    updated

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -43,7 +43,7 @@ resource "aws_route_table" "private_route_table" {
     vpc_id = aws_vpc.project_personal_vpc.id
     route {
         cidr_block = var.private_subnet_cidr
-        gateway_id = aws_internet_gateway.project_personal_igw.id
+        gateway_id = aws_nat_gateway.nat_personal.id
     }
 
     tags = {
@@ -63,19 +63,25 @@ resource "aws_internet_gateway_attachment" "gw_personal_attach" {
     internet_gateway_id = aws_internet_gateway.gw_personal.id
     vpc_id = aws_vpc.project_personal_vpc.id
 }
-
 resource "aws_route_table_association" "pub_sub_to_igw" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
 resource "aws_nat_gateway" "nat_personal" {
-    allocation_id = aws.eip.example.id
+    allocation_id = aws_eip.nat_eip.id
     subnet_id = aws_subnet.private_subnet.id
 
     tags = {
         Name = "gw NAT"
     }
+}
+resource "aws_nat_gateway_eip_association" "nat_eip_assign" {
+  allocation_id  = aws_eip.nat_eip.id
+  nat_gateway_id = aws_nat_gateway.nat_personal.id
 }
 
 resource "aws_route_table_association" "pub_sub_to_natgw" {
