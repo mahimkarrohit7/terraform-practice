e9d1b634f61235c55412b20321b46e04c9e8cbad
mahimkarrohit7 <mahimkarrohit7@gmail.com>
2026-02-28 18:53:58 +0530

    updated core networking service

--- aws-terraform/networking/main.tf
+++ aws-terraform/networking/main.tf
@@ -3,14 +3,14 @@ resource "aws_vpc" "project_personal_vpc" {
     instance_tenancy = "default"
 
     tags = {
-        Name = var.name
+        Name = "${var.name}-vpc"
     }
 }
 resource "aws_internet_gateway" "gw_personal" {
     vpc_id = aws_vpc.project_personal_vpc.id
 
     tags = {
-        Name = "${var.name}-vpc"
+        Name = "${var.name}-IGW"
     }
 }
 resource "aws_subnet" "public_subnet" {
@@ -50,32 +50,29 @@ resource "aws_route_table_association" "pub_sub_to_igw" {
     subnet_id      = aws_subnet.public_subnet[count.index].id
     route_table_id = aws_route_table.public_route_table.id
 }
-resource "aws_internet_gateway_attachment" "gw_personal_attach" {
-    internet_gateway_id = aws_internet_gateway.gw_personal.id
-    vpc_id = aws_vpc.project_personal_vpc.id
-}
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
-resource "aws_nat_gateway" "nat_personal" {
-    allocation_id = aws_eip.nat_eip.id
-    subnet_id = aws_subnet.private_subnet[0].id
-    depends_on = [aws_internet_gateway.gw_personal]
-    tags = {
-        Name = "${var.name}-NATGw"
-    }
-}
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.project_personal_vpc.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_personal.id
-  }
 
-  tags = { Name = "${var.name}-private-rt" }
-}
-resource "aws_route_table_association" "priv_sub_to_natgw" {
-    count = var.az_count
-    subnet_id      = aws_subnet.private_subnet[count.index].id
-    route_table_id = aws_route_table.private_route_table.id
-}
+# resource "aws_eip" "nat_eip" {
+#   domain = "vpc"
+# }
+# resource "aws_nat_gateway" "nat_personal" {
+#     allocation_id = aws_eip.nat_eip.id
+#     subnet_id = aws_subnet.private_subnet[0].id
+#     depends_on = [aws_internet_gateway.gw_personal]
+#     tags = {
+#         Name = "${var.name}-NATGw"
+#     }
+# }
+# resource "aws_route_table" "private_route_table" {
+#   vpc_id = aws_vpc.project_personal_vpc.id
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat_personal.id
+#   }
+
+#   tags = { Name = "${var.name}-private-rt" }
+# }
+# resource "aws_route_table_association" "priv_sub_to_natgw" {
+#     count = var.az_count
+#     subnet_id      = aws_subnet.private_subnet[count.index].id
+#     route_table_id = aws_route_table.private_route_table.id
+# }
